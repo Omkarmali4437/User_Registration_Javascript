621907705f28b1d67349d9b0b9213ef228975dcb
omkarmali4437 <omkarmalli688@gmail.com>
2021-04-19 17:13:34 +0530

    Added logic for validating users first name

--- UserRegistration.js
+++ UserRegistration.js
@@ -1,1 +1,12 @@
+const REGEXNAME = new RegExp("[A-Z]{1}[a-z]{2,}");
+
 console.log("Welcome to User Reegistration Problem");
+
+var digit = require("readline-sync");
+var firstname = digit.question("Enter you First Name: ");
+
+if(REGEXNAME.test(firstname)){
+    console.log("You have entered your first-name correctly");
+}else{
+    console.log("Re-enter your first-name correctly");
+}
