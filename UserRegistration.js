7e4674aafb6ff42e8259198761ba021dfb2a7f19
omkarmali4437 <omkarmalli688@gmail.com>
2021-04-19 17:19:48 +0530

    Added logic for validating users last name

--- UserRegistration.js
+++ UserRegistration.js
@@ -2,11 +2,19 @@ const REGEXNAME = new RegExp("[A-Z]{1}[a-z]{2,}");
 
 console.log("Welcome to User Reegistration Problem");
 
-var digit = require("readline-sync");
-var firstname = digit.question("Enter you First Name: ");
+var string = require("readline-sync");
+var firstname = string.question("Enter you First Name: ");
 
 if(REGEXNAME.test(firstname)){
-    console.log("You have entered your first-name correctly");
+    console.log("You have entered your first-name correctly "+firstname);
 }else{
     console.log("Re-enter your first-name correctly");
+}
+
+var lastname = string.question("Enter your Last Name: ");
+
+if(REGEXNAME.test(lastname)){
+    console.log("You have entered your last-name correctly "+lastname);
+}else{
+    console.log("Re-enter your last-name correctly");
 }
